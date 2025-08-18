808e89af7371ac4d38d57fcf5068782c3968541f
K2bSoares <martinskauane084@gmail.com>
2025-08-18 11:37:16 -0300

    commit arvore kauane

--- 20250814/NodoArvore.java
+++ 20250814/NodoArvore.java
@@ -1,13 +1,12 @@
 public class NodoArvore {
+    
     public NodoArvore filhoEsquerda;
     public int chave;
     public NodoArvore filhoDireita;
 
-    private NodoArvore(int chave) {
+    public NodoArvore(int chave) {
         this.chave = chave;
         this.filhoEsquerda = null;
         this.filhoDireita = null;
     }
-    
-
 }
